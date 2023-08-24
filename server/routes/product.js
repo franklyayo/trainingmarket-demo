238ecc599db8724f24b39c37c7585f7bdc241331
Ayokunle <therealayokunle@gmail.com>
2023-08-24 21:36:49 +0100

    edits

--- server/routes/product.js
+++ server/routes/product.js
@@ -12,7 +12,7 @@ const { auth } = require("../middleware/auth");
 
 var storage = multer.diskStorage({
     destination: (req, file, cb) => {
-        cb(null, '/opt/render/project/src/uploads/')
+        cb(null, 'uploads/')
     },
     filename: (req, file, cb) => {
         cb(null, `${Date.now()}_${file.originalname}`)
