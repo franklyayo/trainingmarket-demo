1543262f1041b3abc99de9b54feb998da28ed8e4
Ayokunle <therealayokunle@gmail.com>
2023-09-01 13:57:35 +0100

    edits

--- server/index.js
+++ server/index.js
@@ -42,7 +42,7 @@ app.use('/api/product', require('./routes/product'));
 //use this to show the image you have in node js server to client (react js)
 //https://stackoverflow.com/questions/48914987/send-image-path-from-node-js-express-server-to-react-client
 //app.use('/opt/render/project/src/uploads', express.static('uploads'));
-app.use('/opt/render/project/src/uploads', express.static('https://thtshop-cv.onrender.com/uploads'));
+app.use('https://thtshop-cv.onrender.com/uploads', express.static('uploads'));
 
 // Serve static assets if in production
 if (process.env.NODE_ENV === "production") {
