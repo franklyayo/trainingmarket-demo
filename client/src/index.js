fb9665a9d92cbf26ecc317a2ff43273665f5bf1e
franklyayo <149245900+franklyayo@users.noreply.github.com>
2025-08-07 13:22:18 +0100

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,7 @@ import 'react-app-polyfill/ie9';
 import 'react-app-polyfill/ie11';
 import 'core-js';
 
-import React from 'react';
+import Reac from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
